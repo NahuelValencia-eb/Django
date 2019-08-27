7e998c7d43f27101bdca1895668253cac57c149a
Nahuel <nahuel.valencia@eventbrite.com>
2019-08-27 14:17:08 -0300

    First commit

---
+++ todolist_site/todolist_app/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+
+# Create your models here.
+class Priority(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=None
+    )
+    done = models.BooleanField(default=False)
---
+++ todolist_site/todolist_app/views.py
@@ -0,0 +1,34 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Task
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+
+# Create your views here.
+class TaskList(ListView):
+    model = Task
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = reverse_lazy('task-list')
+
+
+def check_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    if not task.done:
+        task.done = True
+    task.save()
+    return redirect('task-list')
